04c8a1c9202fc5eed074316a2befe727f29fce87
James Lawn <jl1888@live.co.uk>
2021-02-02 16:41:43 +0000

    page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
+import { SurveyComponent } from './survey/survey.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'survey', component: SurveyComponent},
+  { path: 'results', component: ResultsComponent},
+  { path: '**', component: HomeComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
