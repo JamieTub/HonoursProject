a622cdf76ca8f6da513591f847f14485b108b6e6
James Lawn <jl1888@live.co.uk>
2021-02-02 12:11:00 +0000

    adding more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { environment } from 'src/environments/environment';
+import { ResultsComponent } from './results/results.component';
+import { SurveyComponent } from './survey/survey.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ResultsComponent,
+    SurveyComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
